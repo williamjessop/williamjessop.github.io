1991659be999c387e53dd8482cd00ac3150397e3
william jessop <williamjessop12@outlook.com>
2021-03-20 22:38:55 -0600

    added some cards and tweaked navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/navigation'
+import Home from './components/home'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+            <Route path="/about">
+              <p>about</p>
+            </Route>
+            <Route path="/dashboard">
+              <p>dash</p>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -6,15 +6,19 @@ class Navigation extends React.Component {
         return (
             <div>
                 <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="#home">Will J</Navbar.Brand>
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                    </Nav>
+                    <Navbar.Brand href="/">Will J</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse>
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/about">Link</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                    
                 </Navbar>
             </div>
         );
       }
   }
 
-  export default Navigation;
+export default Navigation;
