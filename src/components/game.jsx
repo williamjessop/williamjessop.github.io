9044ef137d0f13f12126f071fa1ab44e0560f169
William Jessop <williamjessop12@outlook.com>
2021-03-27 14:51:01 -0600

    work on porting game logic and setting up form

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -8,23 +8,29 @@ import pic from "./img/hangman_3.png"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 
+const fs = require('fs');
+
 class Game extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
             strikeNum: 0,
-            correctGuessesString: "",
-            word: "",
+            screenWord: "",
+            word: "test",
             win: false,
-            guesses: []
+            guessedChars: [],
+            input: ""
         };
 
-        this.checkWin = this.checkWin.bind(this);
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     checkWin(word, guessedChars) {
         //this is a quick way to eliminate duplicates in an array. I am using a set to eat identical keys
+        word.replace(/\s/g, '');
         word = [...new Set(word)];
     
         if (word.length !== guessedChars.length) return false;
@@ -36,8 +42,82 @@ class Game extends React.Component {
             if (word[i] !== guessedChars[i]) return false;
     
         return true;
-    }  
+    }
 
+    prepStrikesString(strikes, difficulty, missedChars) {
+        let screenWord = '';
+    
+        for (let i = 0; i < difficulty; i++) {
+            if (strikes !== 0) {
+                screenWord = screenWord + ' X';
+                strikes--;
+            } else {
+                screenWord = screenWord + ' _';
+            }
+        }
+    
+        screenWord = screenWord + ' Missed Letters:'
+    
+        for (let i = 0; i < missedChars.length; i++) {
+            screenWord = screenWord + ` ${missedChars[i]}`
+        }
+    
+        return screenWord + '\n';
+    }
+    
+    updateScreenString() {
+        this.setState((state) => {
+            let something = '';
+        
+            for (let i = 0; i < state.word.length; i++) {
+                if (state.guessedChars.includes(state.word[i]))
+                    something = something + ' ' + state.word[i];
+                else
+                    something = something + ' _';
+            }
+            
+            
+            return {screenWord: something}
+        });
+    }
+
+    updateGuesses(guess){
+        this.setState((state) => {
+            var joined = state.guessedChars.concat([guess]);
+
+            return {
+                guessedChars: joined 
+            }
+        });
+
+        this.updateScreenString();
+    }
+    
+    /*
+    readWordsFromFile() {
+        try {
+            var data = fs.readFileSync('./words.txt', 'utf8')
+            data = data.split('\n');
+            return data;
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    */
+
+    componentDidMount(){
+        this.updateScreenString();
+    }
+
+    handleChange(event){
+        this.setState({input: event.target.value});
+    }
+
+    handleSubmit(event){
+        alert(this.state.input);
+        event.preventDefault();
+        this.setState({input: ''})
+    }
 
     render() {
         return (
@@ -52,23 +132,27 @@ class Game extends React.Component {
                                         <Image src={pic} alt="Some text"/>
                                         
                                         <br/>
-                                        <p>_ E _ _</p>
+                                        
+                                        <p>{this.state.screenWord}</p>
                                     </Col>
                                     <Col>
+                                        
                                         <div style={{color:"red"}}>Incorrect:</div>
                                         <div style={{color:"red"}}>Z X C V</div>
+                                        
+                                        <Button type="submit">Reset</Button>
+                                        
                                     </Col>
                                 </Row>
                                 
                                 <br/>
                                 
-                                <Form style={{display: "flex"}}>
+                                <Form style={{display: "flex"}} onSubmit={this.handleSubmit}>
                                     <Form.Group>
-                                        <Form.Control type="text" placeholder="Put Your Guess Here" />
+                                        <Form.Control type="text" placeholder="Put Your Guess Here" onChange={this.handleChange} value={this.state.input}/>
                                     </Form.Group>
                                     <Button type="submit" style={{height:"70%"}}>Check!</Button>
                                 </Form>
-
                             </Container>
                             
                         </Card.Body>
